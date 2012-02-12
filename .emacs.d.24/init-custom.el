be55a0280669b3f3c7e2994d1188113c476c2d32
Mark Simpson <verdammelt@gmail.com>
2012-02-12 17:22:01 -0500

    custom keeps updating the file - finally sick-and-tired and checking it in

--- .emacs.d.24/init-custom.el
+++ .emacs.d.24/init-custom.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(send-mail-function (quote sendmail-send-it))
-)
+ '(canlock-password "3852e20bef8b9ce61785cd355f16f621d317031e")
+ '(send-mail-function (quote sendmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
