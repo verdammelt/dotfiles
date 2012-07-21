6119d6d1cb84e335433a4488ad421548ec656e3b
Mark Simpson <verdammelt@gmail.com>
2012-07-21 14:27:33 -0400

    Cleaning up formatting of custom variables

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -10,5 +10,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "apple" :family "Inconsolata")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" 
+			 :foreground "white" :inverse-video nil 
+			 :box nil :strike-through nil :overline nil 
+			 :underline nil :slant normal :weight normal 
+			 :height 200 :width normal :foundry "apple" 
+			 :family "Inconsolata")))))
 
