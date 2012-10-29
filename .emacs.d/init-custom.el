b018061939e1b65bc057aaa3639b7c9145e5da28
Mark Simpson <verdammelt@gmail.com>
2012-10-29 16:26:55 -0400

    switching from Inconsolata to Source Code Pro for the default font

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -4,7 +4,7 @@
 ;;;; This file contains the custom-set-variables block. I use customize
 ;;;; as little as possible
 ;;;;
-;;;; Modified Time-stamp: <2012-10-04 06:47:10 mark>
+;;;; Modified Time-stamp: <2012-10-14 16:44:57 mark>
 ;;;;
 ;;;
 (custom-set-variables
@@ -13,16 +13,28 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(canlock-password "3852e20bef8b9ce61785cd355f16f621d317031e")
- '(send-mail-function (quote sendmail-send-it)))
+ '(display-time-mode t)
+ '(send-mail-function (quote sendmail-send-it))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" 
-			 :foreground "white" :inverse-video nil 
-			 :box nil :strike-through nil :overline nil 
-			 :underline nil :slant normal :weight normal 
-			 :height 200 :width normal :foundry "apple" 
-			 :family "Inconsolata")))))
+ '(default ((t (:inherit nil 
+			 :stipple nil 
+			 :background "black" 
+			 :foreground "white" 
+			 :inverse-video nil 
+			 :box nil 
+			 :strike-through nil 
+			 :overline nil 
+			 :underline nil 
+			 :slant normal 
+			 :weight normal 
+			 :height 180 
+			 :width normal 
+			 :foundry "apple" 
+			 :family "Source_Code_Pro")))))
 
