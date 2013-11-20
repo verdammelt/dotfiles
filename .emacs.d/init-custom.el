afe4262a6606e442abcac27d8b0427a12bb8271d
Mark Simpson <verdammelt@gmail.com>
2013-11-19 23:18:49 -0500

    Removing uneeded Customize stuff

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -4,7 +4,7 @@
 ;;;; This file contains the custom-set-variables block. I use customize
 ;;;; as little as possible
 ;;;;
-;;;; Modified Time-stamp: <2013-04-13 20:39:38 mark>
+;;;; Modified Time-stamp: <2013-11-19 23:15:05 mark>
 ;;;;
 ;;;
 (custom-set-variables
@@ -12,11 +12,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(canlock-password "3852e20bef8b9ce61785cd355f16f621d317031e")
- '(display-time-mode t)
- '(send-mail-function (quote sendmail-send-it))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
