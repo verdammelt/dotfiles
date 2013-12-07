bb2c538a77e7131fafaafbd71b61218a25a04320
Mark Simpson <verdammelt@gmail.com>
2013-12-07 16:56:15 -0500

    Making the save-package usage better.
    1. checking in save-packages - should not rely upon packages for this.
    2. changed the saving of the package list - pretty print for better
    diffs.

--- .emacs.d/init-package.el
+++ .emacs.d/init-package.el
@@ -1,3 +1,6 @@
+(require 'save-packages)
+(setq save-packages-file (locate-user-emacs-file ".saved-packages"))
+
 ;;; get Package set up properly and initializedn
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
@@ -6,21 +9,8 @@
 	     :append)
 (package-initialize)
 
-;; bootstrap save-packages if need be 
-(if (not (package-installed-p 'save-packages)) 
-    (progn (message "save-packages not found - assuming the worst")
-	   (package-refresh-contents)
-	   (package-install 'save-packages)))
-(setq save-packages-file (locate-user-emacs-file ".saved-packages"))
 
 ;; report if there are missing packages
-(defun missing-packages (file)
-  (let ((saved-packages 
-	  (car (read-from-string
-		(with-temp-buffer
-		  (insert-file-contents file)
-		  (buffer-string))))))
-    (remove-if 'package-installed-p saved-packages)))
 
 (if (file-exists-p save-packages-file)
     (let ((missing-packages (missing-packages save-packages-file)))
