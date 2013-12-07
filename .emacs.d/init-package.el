d8229505b181cc864c3f06563436f0a46359754a
Mark Simpson <verdammelt@gmail.com>
2013-12-07 16:57:03 -0500

    Switching to MELPA from Marmalade.

--- .emacs.d/init-package.el
+++ .emacs.d/init-package.el
@@ -4,9 +4,8 @@
 ;;; get Package set up properly and initializedn
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives 
-	     '("marmalade" . "http://marmalade-repo.org/packages/") 
-	     :append)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 
