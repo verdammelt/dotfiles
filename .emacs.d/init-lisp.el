9ba21c23766e1e513fb0c91a5c89b0dc43d468f5
Mark Simpson <verdammelt@gmail.com>
2014-01-05 15:00:37 -0500

    Updating Clojure setup.
    nrepl is deprecated - now replaced with cider.

--- .emacs.d/init-lisp.el
+++ .emacs.d/init-lisp.el
@@ -3,7 +3,7 @@
 ;;;;
 ;;;; [if found please return to damned@theworld.com]
 ;;;;
-;;;; Modified Time-stamp: <2013-12-24 22:58:13 mark>
+;;;; Modified Time-stamp: <2013-12-30 19:56:01 mark>
 ;;;;
 
 (setq slime-lisp-implementations
@@ -35,11 +35,13 @@
   (add-hook 'clojure-mode-hook 'paredit-mode)
   (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode))
 
-(after 'nrepl
-  (add-hook 'nrepl-mode-hook 'subword-mode)
-  (add-hook 'nrepl-mode-hook 'paredit-mode)
-  (add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
-  (add-hook 'nrepl-interaction-mode 'nrepl-turn-on-eldoc-mode)
-  (setq nrepl-popup-stacktraces nil))
+(after 'cider
+  (add-hook 'cider-mode-hook 'subword-mode)
+  (add-hook 'cider-mode-hook 'paredit-mode)
+  (add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'cider-interaction-mode 'cider-turn-on-eldoc-mode)
+  (setq nrepl-hide-special-buffers t
+	cider-popup-stacktraces nil
+	cider-repl-popup-stacktraces t))
 
 (provide 'init-lisp)
