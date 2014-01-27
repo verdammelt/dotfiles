b0b2a8a4f69b6c40bb756da3e8d82be2aa2f0554
Mark Simpson <verdammelt@gmail.com>
2014-01-26 23:16:33 -0500

    playing with non-vertical ido-mode

--- .emacs.d/init-ido.el
+++ .emacs.d/init-ido.el
@@ -3,13 +3,13 @@
 ;;;;
 ;;;; [if found please return to damned@theworld.com]
 ;;;;
-;;;; Time-stamp: <2014-01-22 20:10:04 mark>
+;;;; Time-stamp: <2014-01-26 23:16:15 mark>
 ;;;;
 (after 'ido
   (setq ido-show-dot-for-dired t
 	ido-enable-flex-matching t)
 
-  (ido-vertical-mode)
+;  (ido-vertical-mode)
   (ido-sort-mtime-mode))
 
 (provide 'init-ido)
