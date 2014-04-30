3327ffe4ac762c13ccf8db91ff8c8a4d57dae998
Mark Simpson <verdammelt@gmail.com>
2014-04-30 15:32:48 -0400

    Allowing both solarized-light and solarized-dark themes.

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -4,7 +4,7 @@
 ;;;; This file contains the custom-set-variables block. I use customize
 ;;;; as little as possible
 ;;;;
-;;;; Modified Time-stamp: <2014-04-12 19:50:21 mark>
+;;;; Modified Time-stamp: <2014-04-30 15:32:32 mjs>
 ;;;;
 ;;;
 (custom-set-variables
@@ -13,7 +13,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(canlock-password "3852e20bef8b9ce61785cd355f16f621d317031e")
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
