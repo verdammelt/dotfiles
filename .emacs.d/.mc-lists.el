0ab342a4f181a4aaf302b6ef17280d0530f15782
Mark Simpson <verdammelt@gmail.com>
2014-08-28 07:51:54 -0400

    Adding more commands to use with multiple-cursor.

--- .emacs.d/.mc-lists.el
+++ .emacs.d/.mc-lists.el
@@ -3,9 +3,16 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+	backward-to-word
 	forward-sentence
 	ido-hacks-execute-extended-command
 	kill-region
+	kill-sexp
+	paredit-backward
+	paredit-forward-barf-sexp
+	paredit-forward-delete
+	paredit-open-round
+	yas-expand
 	))
 
 (setq mc/cmds-to-run-once
