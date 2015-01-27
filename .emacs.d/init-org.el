87f0c876d9f2955e6e268e219bb53e9b6835babe
Mark Simpson <verdammelt@gmail.com>
2015-01-26 20:13:51 -0500

    Using fullframe to make agenda better.

--- .emacs.d/init-org.el
+++ .emacs.d/init-org.el
@@ -215,14 +215,15 @@
     (org-agenda nil "k")
     (mjs/punch-in))
 
-
   (setq org-stuck-projects
 	'("+CATEGORY=\"PROJ\"+LEVEL=2&-TODO=\"DONE\"" ("TODO" "WAIT") nil ""))
 
   (require 'org-mobile)
   (setq org-mobile-directory "~/Documents/Dropbox/GTD/MobileOrg"
 	org-mobile-inbox-for-pull (expand-file-name "mobileorg.org" org-mobile-directory)
-	))
+	)
+
+  (fullframe org-agenda org-agenda-quit))
 
 
 (global-set-key (kbd "C-c a")  'org-agenda)
