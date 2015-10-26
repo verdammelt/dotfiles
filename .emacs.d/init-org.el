c31d5fe378ee9a976688439ac66823f543a8f1cb
Mark Simpson <verdammelt@gmail.com>
2015-10-25 21:07:21 -0400

    Add custom sorting from http://doc.norang.ca/org-mode.html
    This is just about what I want - with past due items first then
    schedules and deadlines.

--- .emacs.d/init-org.el
+++ .emacs.d/init-org.el
@@ -134,7 +134,7 @@
      org-agenda-todo-ignore-deadlines 'far
 
      org-agenda-sorting-strategy
-     '((agenda time-up scheduled-up deadline-up tag-up todo-state-up alpha-up)
+     '((agenda time-up user-defined-up category-keep)
        (todo todo-state-up tag-up alpha-up)
        (tags todo-state-up tag-up alpha-up)
        (search todo-state-up))
@@ -223,6 +223,107 @@
 
   (setq org-stuck-projects
         '("+CATEGORY=\"PROJ\"+LEVEL=2&-TODO=\"DONE\"" ("TODO" "WAIT") nil ""))
+
+  ;;
+  ;; Agenda sorting functions
+  ;;
+  (setq org-agenda-cmp-user-defined 'bh/agenda-sort)
+
+  ;;
+  ;; ====================
+  ;;
+  ;; From http://doc.norang.ca/org-mode.html
+  ;;
+  (defun bh/agenda-sort (a b)
+    "Sorting strategy for agenda items.
+Late deadlines first, then scheduled, then non-late deadlines"
+    (let (result num-a num-b)
+      (cond
+                                        ; time specific items are already sorted first by org-agenda-sorting-strategy
+
+                                        ; non-deadline and non-scheduled items next
+       ((bh/agenda-sort-test 'bh/is-not-scheduled-or-deadline a b))
+
+                                        ; deadlines for today next
+       ((bh/agenda-sort-test 'bh/is-due-deadline a b))
+
+                                        ; late deadlines next
+       ((bh/agenda-sort-test-num 'bh/is-late-deadline '> a b))
+
+                                        ; scheduled items for today next
+       ((bh/agenda-sort-test 'bh/is-scheduled-today a b))
+
+                                        ; late scheduled items next
+       ((bh/agenda-sort-test-num 'bh/is-scheduled-late '> a b))
+
+                                        ; pending deadlines last
+       ((bh/agenda-sort-test-num 'bh/is-pending-deadline '< a b))
+
+                                        ; finally default to unsorted
+       (t (setq result nil)))
+      result))
+
+  (defmacro bh/agenda-sort-test (fn a b)
+    "Test for agenda sort"
+    `(cond
+                                        ; if both match leave them unsorted
+      ((and (apply ,fn (list ,a))
+            (apply ,fn (list ,b)))
+       (setq result nil))
+                                        ; if a matches put a first
+      ((apply ,fn (list ,a))
+       (setq result -1))
+                                        ; otherwise if b matches put b first
+      ((apply ,fn (list ,b))
+       (setq result 1))
+                                        ; if none match leave them unsorted
+      (t nil)))
+
+  (defmacro bh/agenda-sort-test-num (fn compfn a b)
+    `(cond
+      ((apply ,fn (list ,a))
+       (setq num-a (string-to-number (match-string 1 ,a)))
+       (if (apply ,fn (list ,b))
+           (progn
+             (setq num-b (string-to-number (match-string 1 ,b)))
+             (setq result (if (apply ,compfn (list num-a num-b))
+                              -1
+                            1)))
+         (setq result -1)))
+      ((apply ,fn (list ,b))
+       (setq result 1))
+      (t nil)))
+
+  (defun bh/is-not-scheduled-or-deadline (date-str)
+    (and (not (bh/is-deadline date-str))
+         (not (bh/is-scheduled date-str))))
+
+  (defun bh/is-due-deadline (date-str)
+    (string-match "Deadline:" date-str))
+
+  (defun bh/is-late-deadline (date-str)
+    (string-match "\\([0-9]*\\) d\. ago:" date-str))
+
+  (defun bh/is-pending-deadline (date-str)
+    (string-match "In \\([^-]*\\)d\.:" date-str))
+
+  (defun bh/is-deadline (date-str)
+    (or (bh/is-due-deadline date-str)
+        (bh/is-late-deadline date-str)
+        (bh/is-pending-deadline date-str)))
+
+  (defun bh/is-scheduled (date-str)
+    (or (bh/is-scheduled-today date-str)
+        (bh/is-scheduled-late date-str)))
+
+  (defun bh/is-scheduled-today (date-str)
+    (string-match "Scheduled:" date-str))
+
+  (defun bh/is-scheduled-late (date-str)
+    (string-match "Sched\.\\(.*\\)x:" date-str))
+  ;;
+  ;; ====================
+  ;;
 
   (fullframe org-agenda org-agenda-quit))
 
