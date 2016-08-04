84f2b3568c9926d9113d09375e3dd78da185c68e
Mark Simpson <verdammelt@gmail.com>
2016-08-03 21:56:38 -0400

    Tweaking company mode setup.
    Colors and keybindings.

--- .emacs.d/init-misc.el
+++ .emacs.d/init-misc.el
@@ -69,6 +69,20 @@
   (add-hook 'python-mode-hook #'(lambda () (setq fill-column 79))))
 
 (with-eval-after-load 'company
+  (set-face-attribute 'company-tooltip nil :background "white" :foreground "black")
+  (set-face-attribute 'company-tooltip-selection nil :background "grey" :foreground "red")
+  (set-face-attribute 'company-tooltip-common nil :slant 'italic :foreground "blue")
+  (set-face-attribute 'company-scrollbar-fg nil :background "black")
+  (set-face-attribute 'company-scrollbar-bg nil :background "grey")
+  (defvar company-active-map)
+  (defvar company-idle-delay)
+  (defvar company-show-numbers)
+  (defvar company-selection-wrap-around)
+  (define-key company-active-map (kbd "\C-n") 'company-select-next)
+  (define-key company-active-map (kbd "\C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
+  (define-key company-active-map (kbd "M-.") 'company-show-location)
+  (diminish 'company)
   (setq company-idle-delay .25
         company-show-numbers t
         company-selection-wrap-around t))
