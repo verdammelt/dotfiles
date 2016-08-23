2030097014f5f6b2eff8436d726cafdb9f787bb3
Mark Simpson <verdammelt@gmail.com>
2016-08-23 10:15:33 -0400

    Use NVM to add node version to path.
    Used as a projectile-switch-project-hook function. Will use the
    version specified in current directory's .nvmrc file if found
    otherwise pick one from the installed list.

--- .emacs.d/init-javascript.el
+++ .emacs.d/init-javascript.el
@@ -1,15 +1,28 @@
-(require 'nvm)
-(nvm-use (caar (last (nvm--installed-versions)))) ; cavalierly use last one
-(add-to-list 'exec-path (getenv "NVM_BIN"))
+(autoload 'nvm-use "nvm")
+(autoload 'nvm-use-for "nvm")
+(autoload 'nvm--installed-versions "nvm")
 
-(with-eval-after-load 'projectile
-  (add-hook 'projectile-after-switch-project-hook 'mjs/add-node-modules-in-path))
+(defvar mjs/previous-node-version nil)
+(defun mjs/choose-node-version ()
+  "Choose and use the correct version of node for this current
+directory. If there is a .nvmrc file use that - otherwise pick
+one of the installed versions (arbitrarily: the last)."
+  (when mjs/previous-node-version
+    (setq exec-path
+          (cl-remove mjs/previous-node-version exec-path
+                     :test #'string=)
+          mjs/previous-node-version nil))
+  (if (file-exists-p ".nvmrc")
+      (nvm-use-for ".")
+    (nvm-use (caar (last (nvm--installed-versions)))))
+  (setq mjs/previous-node-version (getenv "NVM_BIN")
+        exec-path (cl-pushnew mjs/previous-node-version exec-path
+                              :test #'string=)))
 
-(defvar mjs/project-node-module-special-cases (list))
-(add-to-list 'mjs/project-node-module-special-cases "VestaWeb")
-
+(defvar mjs/project-node-module-special-cases (list)
+  "Some projects may not have their node_modules directory at
+  their top level.")
 (defvar mjs/previous-node-modules-added-to-path nil)
-
 (defun mjs/add-node-modules-in-path ()
   "I don't install project dependencies globally so I need to add
 the .node_modules/.bin directory to the exec path. Sometimes the
@@ -25,11 +38,13 @@ mjs/project-node-module-special-cases."
     (when mjs/previous-node-modules-added-to-path
       (setq exec-path
             (cl-remove mjs/previous-node-modules-added-to-path exec-path
-                       :test #'string-equal)
+                       :test #'string=)
             mjs/previous-node-modules-added-to-path nil))
     (when node-modules-bind-dir
-      (setq mjs/previous-node-modules-added-to-path node-modules-bind-dir)
-      (cl-pushnew node-modules-bind-dir exec-path))))
+      (setq mjs/previous-node-modules-added-to-path node-modules-bind-dir
+            exec-path (cl-pushnew node-modules-bind-dir exec-path
+                                  :test #'string=)))))
+
 
 (add-to-list 'auto-mode-alist '("\\.jsx?$" . js2-jsx-mode))
 (with-eval-after-load 'js2-mode
@@ -58,3 +73,9 @@ mjs/project-node-module-special-cases."
     (tide-setup)
     (eldoc-mode +1))
   (add-hook 'typescript-mode-hook 'setup-tide))
+
+(with-eval-after-load 'projectile
+  (add-hook 'projectile-after-switch-project-hook 'mjs/add-node-modules-in-path)
+  (add-hook 'projectile-after-switch-project-hook 'mjs/choose-node-version))
+
+(mjs/choose-node-version)
