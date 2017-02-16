1a71dad1ad3c4741b4ea414d235bdc3d5e2d29ce
Mark Simpson <verdammelt@gmail.com>
2017-02-15 23:26:00 -0500

    Revert agenda scheduled setup.
    It was not working as expected. It was not keeping scheduled items out
    of tags-todo agenda views.

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -16,7 +16,7 @@
     ("5999e12c8070b9090a2a1bbcd02ec28906e150bb2cdce5ace4f965c76cf30476" "9cb6358979981949d1ae9da907a5d38fb6cde1776e8956a1db150925f2dad6c1" "8abee8a14e028101f90a2d314f1b03bed1cde7fd3f1eb945ada6ffc15b1d7d65" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (auctex auto-complete avy bbdb browse-kill-ring change-inner cider clj-refactor company-dict company-quickhelp diminish edit-server-htmlize elisp-slime-nav fill-column-indicator flx-ido fullframe haml-mode ido-hacks ido-sort-mtime ido-ubiquitous ido-vertical-mode js2-refactor keyfreq magit markdown-mode miniedit nvm org-plus-contrib projectile-rails rainbow-delimiters rvm scss-mode sicp slime-company smex tango-2-theme tide virtualenvwrapper wgrep wrap-region yaml-mode))))
+    (auctex auto-complete avy bbdb browse-kill-ring change-inner cider clj-refactor company-dict company-quickhelp diminish edit-server-htmlize elisp-slime-nav fill-column-indicator flx-ido flycheck-credo fullframe haml-mode ido-hacks ido-sort-mtime ido-ubiquitous ido-vertical-mode js2-refactor keyfreq magit magithub markdown-mode miniedit nvm org-plus-contrib projectile-rails rainbow-delimiters rvm scss-mode sicp slime-company smex tango-2-theme tide virtualenvwrapper wgrep wrap-region yaml-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
