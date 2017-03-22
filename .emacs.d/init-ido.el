6b108db2c05989a782ff42c9f6d85e5548735639
Mark Simpson <verdammelt@gmail.com>
2017-03-21 21:49:30 -0400

    Switch init-ido to use-package.

--- .emacs.d/init-ido.el
+++ .emacs.d/init-ido.el
@@ -3,17 +3,27 @@
 ;;;;
 ;;;; [if found please return to damned@theworld.com]
 ;;;;
-(with-eval-after-load 'ido
-  (setq ido-show-dot-for-dired t
-        ido-enable-flex-matching t
-        ido-vertical-define-keys t)
-  (ido-vertical-mode)
-  (ido-sort-mtime-mode))
+(use-package ido
+  :commands (ido-everywhere)
+  :config
+  (progn
+    (setq ido-show-dot-for-dired t
+          ido-enable-flex-matching t)
+    (ido-vertical-mode)
+    (ido-sort-mtime-mode)))
 
-(autoload 'ido-hacks-mode "ido-hacks")
-(defun mjs/setup-smex ()
-  (global-set-key (kbd "M-x") 'smex))
-(add-hook 'ido-hacks-mode-hook 'mjs/setup-smex)
+(use-package ido-vertical-mode
+  :config
+  (setq ido-vertical-define-keys t))
+
+(use-package ido-hacks
+  :commands (ido-hacks-mode)
+  :bind (("M-x" . smex)))
+
+(use-package ido-sort-mtime)
+(use-package ido-ubiquitous)
+(use-package flx-ido)
+(use-package smex)
 
 (defun mjs/turn-on-ido ()
   (ido-mode)
@@ -21,4 +31,4 @@
   (ido-ubiquitous-mode)
   (ido-hacks-mode)
   (flx-ido-mode 1)
-  (smex-initialize))			; must be after ido-hacks!
+  (smex-initialize)) ;; must be after ido-hacks!
