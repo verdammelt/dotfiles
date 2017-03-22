9979acfe205c20ead30b9c001db512e165223d96
Mark Simpson <verdammelt@gmail.com>
2017-03-21 22:06:18 -0400

    Update use-package usage in init-prog-elixir.

--- .emacs.d/init-prog-elixir.el
+++ .emacs.d/init-prog-elixir.el
@@ -1,15 +1,8 @@
 (use-package elixir-mode
-  :defer t
   :config (add-hook 'elixir-mode-hook 'alchemist-mode))
-
 (use-package alchemist
-  :defer t
   :diminish (alchemist-mode alchemist-phoenix-mode))
-
-(use-package elixir-yasnippets :defer t)
-
-(use-package flycheck-elixir
-  :defer t
-  :init (setq flycheck-elixir-credo-strict t))
-
-(use-package flycheck-credo :defer t)
+(use-package elixir-yasnippets)
+(use-package flycheck-elixir)
+(use-package flycheck-credo
+  :config (setq flycheck-elixir-credo-strict t))
