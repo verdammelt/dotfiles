f367b6f3e1602aa38408572a8401230f0ea93c8e
Mark Simpson <verdammelt@gmail.com>
2017-05-31 09:05:45 -0400

    Update init-latex to use use-package.

--- .emacs.d/init-latex.el
+++ .emacs.d/init-latex.el
@@ -3,17 +3,24 @@
 ;;;;
 ;;;; [if found please return to damned@theworld.com]
 ;;;;
-(with-eval-after-load 'tex
-  (TeX-global-PDF-mode 1)
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
-  (setq-default TeX-master nil))
+(use-package tex
+  :ensure nil
+  :functions (TeX-global-PDF-mode)
+  :init (setq-default TeX-master nil)
+  :config
+  (progn (TeX-global-PDF-mode 1)
+         (setq TeX-auto-save t
+               TeX-parse-self t)))
 
-(with-eval-after-load 'latex
-  (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
-  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
-  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(use-package latex
+  :ensure nil
+  :config
+  (progn
+    (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+    (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+    (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+    (add-hook 'LaTeX-mode-hook 'turn-on-reftex)))
 
-  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-  (with-eval-after-load 'reftex
-    (setq reftex-plug-into-AUCTeX t)))
+(use-package reftex
+  :ensure nil
+  :config (setq reftex-plug-into-AUCTeX t))
