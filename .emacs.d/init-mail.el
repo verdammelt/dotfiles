5522b7eb782c688d70112cd7f542d311324575f7
Mark Simpson <verdammelt@gmail.com>
2017-05-31 09:04:24 -0400

    Update init-mail to use use-package.

--- .emacs.d/init-mail.el
+++ .emacs.d/init-mail.el
@@ -8,52 +8,60 @@
 ;; this way i have Gcc: etc. in my mail buffer
 (setq mail-user-agent 'gnus-user-agent)
 
-;; getting bbdb in my message setup
-(with-eval-after-load 'message
-  (mjs/bbdb-init 'message)
+(use-package message
+  :ensure nil
+  :config
+  (progn
+    (mjs/bbdb-init 'message)
 
-  (autoload 'sm-add-random-header "silly-mail" nil t)
+    ;; extra stuff for message buffers - spelling etc.
+    (add-hook 'message-setup-hook 'bbdb-mail-aliases)
+    (add-hook 'message-setup-hook 'footnote-mode)
+    (add-hook 'message-setup-hook 'message-sort-headers)
+    (add-hook 'message-setup-hook 'mml-secure-message-sign)
+    (add-hook 'message-setup-hook 'sm-add-random-header)
+    (add-hook 'message-setup-hook 'turn-on-flyspell)
+    (add-hook 'message-send-hook 'ispell-message)
 
-  (add-hook 'message-setup-hook 'bbdb-mail-aliases)
+    (setq message-citation-line-function 'message-insert-formatted-citation-line
+          message-default-headers "X-Attribution: MJS"
+          message-sendmail-envelope-from 'header)))
 
-  (setq message-citation-line-function 'message-insert-formatted-citation-line)
+(use-package mm-decode
+  :ensure nil
+  :config
+  (progn
+    (setq mm-default-directory (expand-file-name "~/Downloads")
+          mm-verify-option 'always
+          mm-decrypt-option 'ask
+          mm-sign-option nil
+          mm-encrypt-option 'guided
+          mml-secure-openpgp-encrypt-to-self t
+          mml-secure-openpgp-sign-with-sender t)
+    (add-to-list 'mm-file-name-rewrite-functions
+                 'mm-file-name-replace-whitespace)))
 
-  ;; extra stuff for message buffers - spelling etc.
-  (add-hook 'message-setup-hook 'message-sort-headers)
-  (add-hook 'message-setup-hook 'footnote-mode)
-  (add-hook 'message-setup-hook 'turn-on-flyspell)
-  (add-hook 'message-setup-hook 'sm-add-random-header)
-  (add-hook 'message-send-hook 'ispell-message)
-  (add-hook 'message-setup-hook 'mml-secure-message-sign)
+(use-package silly-mail
+  :ensure nil
+  :commands (sm-add-random-header)
+  :defines (sm-mail-header-table)
+  :config
+  (progn
+   (setq sm-mail-header-table
+         '(sm-add-emacs-name
+           sm-add-emacs-taunt
+           (sm-add-horoscope           horoscope           "horoscope")
+           (sm-add-kibology            kibologize          "kibologize")
+           sm-add-meat
+           (sm-add-shopping-list       shop-string         "shop")
+           sm-add-tom-swifty
+           sm-add-tomato
+           (sm-add-uboat-death-message uboat-death-message "uboat")
+           sm-add-zippy-quote))))
 
-  (setq message-default-headers "X-Attribution: MJS")
-
-  (setq message-sendmail-envelope-from 'header))
-
-(with-eval-after-load 'mm-decode
-  (setq mm-default-directory (expand-file-name "~/Downloads"))
-  (setq mm-verify-option 'always
-        mm-decrypt-option 'ask
-        mm-sign-option nil
-        mm-encrypt-option 'guided
-        mml-secure-openpgp-encrypt-to-self t
-        mml-secure-openpgp-sign-with-sender t)
-  (add-to-list 'mm-file-name-rewrite-functions 'mm-file-name-replace-whitespace))
-
-(with-eval-after-load 'silly-mail
-  (setq sm-mail-header-table
-        '(sm-add-emacs-name
-          sm-add-emacs-taunt
-          (sm-add-horoscope           horoscope           "horoscope")
-          (sm-add-kibology            kibologize          "kibologize")
-          sm-add-meat
-          (sm-add-shopping-list       shop-string         "shop")
-          sm-add-tom-swifty
-          sm-add-tomato
-          (sm-add-uboat-death-message uboat-death-message "uboat")
-          sm-add-zippy-quote)))
-
-(with-eval-after-load 'sendmail
-  (setq send-mail-function 'sendmail-send-it)
-  (setq mail-specify-envelope-from t
-        mail-envelope-from 'header))
+(use-package sendmail
+  :config
+  (progn
+    (setq send-mail-function 'sendmail-send-it
+          mail-specify-envelope-from t
+          mail-envelope-from 'header)))
