e9512861bdba1accd30388d04dd4056220164f5f
Mark Simpson <verdammelt@gmail.com>
2017-06-15 18:07:47 -0400

    Update Typescript mode.

--- .emacs.d/init-prog-typescript.el
+++ .emacs.d/init-prog-typescript.el
@@ -1,9 +1,21 @@
 (use-package typescript-mode
   :config
-  (progn (setq typescript-indent-level 2)
-         (add-hook 'typescript-mode-hook 'mjs/setup-tide)))
+  (progn (setq typescript-indent-level 4)
+         (add-hook 'typescript-mode-hook 'mjs/setup-tide)
+         (add-hook 'projectile-after-switch-project-hook
+                   'mjs/setup-tslint)))
 
-(use-package tide)
+(defun mjs/setup-tslint ()
+  (let ((project-root (projectile-project-root)))
+    (setq flycheck-typescript-tslint-config
+          (expand-file-name "VestaWeb/tslint.json" project-root)
+          flycheck-typescript-tslint-executable
+          (expand-file-name "VestaWeb/node_modules/.bin/tslint" project-root))))
+
+(use-package tide
+  :config
+  (setq tide-format-options
+        '(:indentSize 4 :tabSize 4)))
 
 (defun mjs/setup-tide ()
   (tide-setup)
