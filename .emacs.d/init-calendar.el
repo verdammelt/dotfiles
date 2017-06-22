4099b101f472b8d05761dbfc46cf9ecd9f90a205
Mark Simpson <verdammelt@gmail.com>
2017-06-22 19:21:40 -0400

    Using use-package for calendar.

--- .emacs.d/init-calendar.el
+++ .emacs.d/init-calendar.el
@@ -1,37 +1,57 @@
-;; time display the way i like it
-(with-eval-after-load 'time
-  (setq display-time-24hr-format t
-        display-time-day-and-date t
-        display-time-use-mail-icon t
-        display-time-mail-face 'cursor ; (only background color used)
-        display-time-format "%Y-%m-%dT%R"))
+(use-package time
+  :ensure nil
+  :init
+  (progn
+    (defvar display-time-24hr-format)
+    (defvar display-time-use-mail-icon)
+    (defvar display-time-mail-face)
+    (defvar display-time-format)
+    (setq display-time-24hr-format t
+          display-time-day-and-date t
+          display-time-use-mail-icon t
+          display-time-mail-face 'cursor ; (only background color used)
+          display-time-format "%Y-%m-%dT%R")
+    (display-time)))
 
-;; calendar
-(setq holiday-hebrew-holidays nil
-      holiday-islamic-holidays nil
-      holiday-oriental-holidays nil
-      holiday-bahai-holidays nil)
-(setq holiday-other-holidays
-      '((holiday-sexp '(if (zerop (% year 4))
-                           (calendar-gregorian-from-absolute
-                            (1+ (calendar-dayname-on-or-before
-                                 1 (+ 6 (calendar-absolute-from-gregorian
-                                         (list 11 1 year)))))))
-                      "US Presidential Election")))
-(with-eval-after-load 'calendar
-  (add-hook 'diary-list-entries-hook 'diary-include-other-diary-files)
-  (add-hook 'diary-mark-entries-hook 'diary-mark-included-diary-files)
+(use-package diary
+  :ensure nil
+  :init
+  (progn
+    (add-hook 'diary-list-entries-hook 'diary-include-other-diary-files)
+    (add-hook 'diary-mark-entries-hook 'diary-mark-included-diary-files)))
 
-  (setq
-   calendar-latitude +40.72541
-   calendar-longitude -73.70928
-   calendar-location-name "Floral Park, NY"
-   calendar-time-display-form '(24-hours ":" minutes (if time-zone " (") time-zone (if time-zone ")"))
-   diary-file "~/.diary"
-   calendar-mark-diary-entries-flag t
-   calendar-mark-holidays-flag t
-   calendar-view-diary-initially-flag t
-   calendar-view-holidays-initially-flag t)
-  (calendar-set-date-style 'iso))
+(use-package holiday
+  :ensure nil
+  :init
+  (setq holiday-hebrew-holidays nil
+        holiday-islamic-holidays nil
+        holiday-oriental-holidays nil
+        holiday-bahai-holidays nil
+        holiday-other-holidays
+        '((holiday-sexp '(if (zerop (% year 4))
+                             (calendar-gregorian-from-absolute
+                              (1+ (calendar-dayname-on-or-before
+                                   1 (+ 6 (calendar-absolute-from-gregorian
+                                           (list 11 1 year)))))))
+                        "US Presidential Election"))))
 
-(display-time)
+(use-package calendar
+  :ensure nil
+  :commands (calendar-set-date-style)
+  :init (setq
+         diary-file "~/.diary"
+         calendar-mark-diary-entries-flag t
+         calendar-mark-holidays-flag t
+         calendar-view-diary-initially-flag t
+         calendar-view-holidays-initially-flag t)
+  :config (calendar-set-date-style 'iso))
+
+(use-package solar
+  :ensure nil
+  :config
+  (setq calendar-latitude +40.72541
+        calendar-longitude -73.70928
+        calendar-location-name "Floral Park, NY"
+        calendar-time-display-form
+        '(24-hours ":" minutes
+                   (if time-zone " (") time-zone (if time-zone ")"))))
