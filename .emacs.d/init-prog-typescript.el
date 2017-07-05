8609546ebdb5a7291d6dcf16ed404ad21f439fa5
Mark Simpson <verdammelt@gmail.com>
2017-07-04 21:55:36 -0400

    Use use-packag for programming modes.

--- .emacs.d/init-prog-typescript.el
+++ .emacs.d/init-prog-typescript.el
@@ -1,10 +1,6 @@
-(use-package typescript-mode
-  :config
-  (progn (setq typescript-indent-level 4)
-         (add-hook 'typescript-mode-hook 'mjs/setup-tide)
-         (add-hook 'projectile-after-switch-project-hook
-                   'mjs/setup-tslint)))
-
+(declare-function projectile-project-root "projectile")
+(defvar flycheck-typescript-tslint-config)
+(defvar flycheck-typescript-tslint-executable)
 (defun mjs/setup-tslint ()
   (let ((project-root (projectile-project-root)))
     (setq flycheck-typescript-tslint-config
@@ -12,11 +8,18 @@
           flycheck-typescript-tslint-executable
           (expand-file-name "VestaWeb/node_modules/.bin/tslint" project-root))))
 
+(defun mjs/setup-tide ()
+  (tide-setup)
+  (eldoc-mode +1))
+
+(use-package typescript-mode
+  :config
+  (progn (setq typescript-indent-level 4)
+         (add-hook 'typescript-mode-hook 'mjs/setup-tide)
+         (add-hook 'projectile-after-switch-project-hook
+                   'mjs/setup-tslint)))
+
 (use-package tide
   :config
   (setq tide-format-options
         '(:indentSize 4 :tabSize 4)))
-
-(defun mjs/setup-tide ()
-  (tide-setup)
-  (eldoc-mode +1))
--- .emacs.d/init-prog.el
+++ .emacs.d/init-prog.el
@@ -1,48 +1,49 @@
 (use-package cc-vars
   :ensure nil
-  :init (setq c-default-style "linux"
-              c-basic-offset 4))
+  :init (setq-default c-default-style "linux"
+                      c-basic-offset 4))
 
 ;; editing programs
 (defun mjs/prog-fill-column ()
   (setq fill-column 80))
 
-(with-eval-after-load 'simple
-  (add-hook 'prog-mode-hook 'mjs/prog-fill-column)
-  (add-hook 'prog-mode-hook 'linum-mode)
-  (add-hook 'prog-mode-hook 'fci-mode)
-  (add-hook 'prog-mode-hook 'whitespace-mode))
+(use-package simple
+  :ensure nil
+  :config (progn
+            (add-hook 'prog-mode-hook 'mjs/prog-fill-column)
+            (add-hook 'prog-mode-hook 'linum-mode)
+            (add-hook 'prog-mode-hook 'fci-mode)
+            (add-hook 'prog-mode-hook 'whitespace-mode)))
 
 (use-package prog-mode
   :ensure nil
   :init (global-prettify-symbols-mode))
 
+(use-package python
+  :config (add-hook 'python-mode-hook #'(lambda () (setq fill-column 79))))
+(use-package virtualenvwrapper
+  :config (venv-initialize-interactive-shells))
 
-(with-eval-after-load 'python
-  (add-hook 'python-mode-hook #'(lambda () (setq fill-column 79))))
-(venv-initialize-interactive-shells)
-
-(with-eval-after-load 'ruby
-  (setq ruby-insert-encoding-magic-comment nil))
+(use-package ruby-mode
+  :config (setq ruby-insert-encoding-magic-comment nil))
 
 (use-package rvm
   :defer 2
   :config (rvm-use-default))
 
-(with-eval-after-load 'compile
-  (defvar compilation-filter-start)
-  (defvar compilation-scroll-output)
+(use-package ansi-color
+  :ensure nil
+  :commands (ansi-color-apply-on-region))
 
-  (setq compilation-scroll-output 'first-error)
+(defvar compilation-filter-start)
+(defun mjs/colorize-compilation ()
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region
+     compilation-filter-start (point))))
 
-  (require 'ansi-color)
-
-  (defun mjs/colorize-compilation ()
-    (let ((inhibit-read-only t))
-      (ansi-color-apply-on-region
-       compilation-filter-start (point))))
-
-  (add-hook 'compilation-filter-hook
-            #'mjs/colorize-compilation))
+(use-package compile
+  :config
+  (progn (setq compilation-scroll-output 'first-error)
+         (add-hook 'compilation-filter-hook #'mjs/colorize-compilation)))
 
 (use-package less-css-mode)
