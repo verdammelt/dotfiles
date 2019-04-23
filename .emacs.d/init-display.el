bc9d1b4bc8b01f9393890f4cc3bbe55857768b5e
Mark Simpson <verdammelt@gmail.com>
2019-04-22 21:08:03 -0400

    WIP: mode-line tweaking

--- .emacs.d/init-display.el
+++ .emacs.d/init-display.el
@@ -13,5 +13,76 @@
                     :height 180
                     :family "DejaVu Sans Mono")
 
-(load-theme 'tango-2)
+(load-theme 'tango-dark)
+(set-background-color "black")
+(set-face-attribute 'highlight nil :background "thistle")
+
+(cl-defun mjs/bg-color-box (face &optional (width 5))
+  (list :line-width width :color (face-attribute face :background)))
+
+(set-face-attribute 'mode-line nil
+                    :box (mjs/bg-color-box 'mode-line))
+
+(set-face-attribute 'mode-line-inactive nil
+                    :box (mjs/bg-color-box 'mode-line-inactive))
+
+;; (setq-default mode-line-format
+;;               (list
+;;                (propertize (format-time-string "<%Y-%m-%dT%H:%M>")
+;;                            'face 'font-lock-builtin-face)
+;;                " "
+;;                "%b"
+;;                " "
+;;                "("
+;;                (propertize "%02l" 'face 'font-lock-keyword-face)
+;;                ","
+;;                (propertize "%02c" 'face 'font-lock-keyword-face)
+;;                ")"
+;;                " "
+;;                "["
+;;                (propertize "%p" 'face 'font-lock-keyword-face) ;; % above top
+;;                "/"
+;;                (propertize "%I" 'face 'font-lock-keyword-face) ;; size
+;;                "]"
+;;                '(:eval (propertize
+;;                         " "
+;;                         'display
+;;                         `((space :align-to (- (+ right right-fringe right-margin)
+;;                                               ,(+ 2 (string-width mode-name)))))))
+;;                (propertize "%m" 'face 'font-lock-constant-face)
+;;                ))
+
+
+;; (setq-default mode-line-format
+;;               '("%e"
+;;                 mode-line-front-space
+;;                 mode-line-mule-info
+;;                 mode-line-client
+;;                 mode-line-modified
+;;                 mode-line-remote
+;;                 mode-line-frame-identification
+;;                 ;; mode-line-buffer-identification
+;;                 ;; "   "
+;;                 ;; mode-line-position
+;;                 vc-mode
+;;                 "  "
+;;                 mode-line-misc-info
+;;                 mode-line-end-spaces
+
+;;                 ;; (propertize "foo"
+;;                 ;;             'face 'font-lock-doc-face)
+
+;;                 ;; '(:eval (propertize
+;;                 ;;          " "
+;;                 ;;          'display
+;;                 ;;          `((space :align-to (- (+ right right-fringe right-margin)
+;;                 ;;                                ,(+ 3 (string-width mode-name)))))))
+;;                 ;; '(:eval (propertize
+;;                 ;;           " "
+;;                 ;;           'display
+;;                 ;;           `((space :align-to (- (+ right right-fringe right-margin)
+;;                 ;;                                 (+ 3 4))))))
+;;                 ;; (propertize mode-name 'face 'font-lock-constant-face)
+;;                 ))
+
 (set-cursor-color "red")
