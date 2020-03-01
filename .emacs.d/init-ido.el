faf2e83a25c0636bc08e4a32e4c36d8520800f98
Mark Simpson <verdammelt@gmail.com>
2020-03-01 12:05:39 -0500

    Simply ido setup.

--- .emacs.d/init-ido.el
+++ .emacs.d/init-ido.el
@@ -3,35 +3,20 @@
 ;;;;
 ;;;; [if found please return to damned@theworld.com]
 ;;;;
-(defun mjs/ido-init ()
-  (ido-vertical-mode)
-  (ido-sort-mtime-mode)
-
-  (ido-mode)
-  (ido-everywhere)
-  (ido-ubiquitous-mode)
-  (ido-hacks-mode)
-  (flx-ido-mode 1)
-  (smex-initialize))
-
 (use-package ido
-  :commands (ido-everywhere)
-  :init (add-hook 'after-init-hook 'mjs/ido-init t)
+  :commands (ido-everywhere ido-next-match ido-prev-match ido-toggle-prefix)
+  :hook ((after-init . ido-mode)
+         (after-init . ido-everywhere))
+  :bind (:map ido-common-completion-map
+              (("\C-n" . 'ido-next-match)
+               ("\C-p" . 'ido-prev-match)
+               ("C-S-p" . 'ido-toggle-prefix)))
   :config
   (progn
     (setq ido-show-dot-for-dired t
-          ido-enable-flex-matching t)))
+          ido-enable-flex-matching t
+          ido-use-filename-at-point 'guess
+          ido-use-url-at-point t)))
 
-(use-package ido-vertical-mode
-  :config
-  (setq ido-vertical-define-keys t))
-
-(use-package ido-hacks
-  :commands (ido-hacks-mode)
-  :bind (("M-x" . smex)))
-
-(use-package ido-sort-mtime)
-(use-package ido-completing-read+)
-(use-package flx-ido)
-(use-package smex
-  :config (setq smex-save-file (locate-user-emacs-file ".smex-items")))
+(use-package ido-completing-read+
+  :hook ((after-init . ido-ubiquitous-mode)))
