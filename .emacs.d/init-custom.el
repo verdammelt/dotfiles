8cede46ca999d14808a5d3af3afafc68bbc1dcf3
Mark Simpson <verdammelt@gmail.com>
2020-05-02 17:00:12 -0400

    Updates to package list & safe-themes

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -13,11 +13,11 @@
  '(canlock-password "3852e20bef8b9ce61785cd355f16f621d317031e")
  '(custom-safe-themes
    (quote
-    ("575d772a465e51f9ba7dd9c6213275c7aa3dc68ede1692dcd1521e5d70a7f58d" "d574db69fcc4cc241cb4a059711791fd537a959d8b75f038913639e8e006ca48")))
+    ("e5b6c8ffafbb4ca46fdc6481d47ad6132664fd1f7452b0ed8efb7104cb568401" "0be2808c8b98ad9774a1066a8c74cde6bf5a19a4de1984c43ed3934c854e14f7" "c6a6eae2c9b1398b03666a8951757d45bda7a2990c82d7c85d1fb4ab9691f916" "5c22313c9fca4889f8db919531af81dcd42a8127410be1a302579497fdc10705" "e5b947fab0dd7f8eb34d43d88556d8c6c15db4f404a20f31d2affb56ad15aa6b" "f81f3275a7b64bf010d3956191893861dd6d0fb7db0c8053e9698ad39fc3d9cc" "b29772dde9e90011e63c0da1a151f19ff4c1fed49a75d8280c188786093b133b" "575d772a465e51f9ba7dd9c6213275c7aa3dc68ede1692dcd1521e5d70a7f58d" "d574db69fcc4cc241cb4a059711791fd537a959d8b75f038913639e8e006ca48")))
  '(gnus-group-change-level-function (quote nnreddit-update-subscription))
  '(package-selected-packages
    (quote
-    (alchemist auctex bbdb browse-kill-ring change-inner cider cider-repl clj-refactor color-theme-sanityinc-tomorrow company company-dict company-go csharp-mode diminish edit-server-htmlize elisp-slime-nav elixir-credo elixir-mode elixir-yasnippets fill-column-indicator flycheck-credo flycheck-elixir fullframe go-dlv go-eldoc go-guru go-mode go-snippets haml-mode ido-completing-read+ ido-ubiquitous js2-refactor latex less-css-mode lisp-mode magit magithub markdown-mode message minibuffer miniedit mm-decode modus-operandi-theme modus-vivendi-theme nnreddit nvm org-plus-contrib prettier-js prog-mode projectile-rails protobuf-mode rainbow-delimiters rjsx-mode rvm scss-mode sicp silly-mail simple sly tex tide use-package virtualenvwrapper web-mode wgrep wrap-region yaml-mode yasnippet-snippets))))
+    (alchemist auctex bbdb browse-kill-ring change-inner cider cider-repl clj-refactor color-theme-sanityinc-tomorrow company company-go csharp-mode diminish edit-server-htmlize elisp-slime-nav elixir-credo elixir-mode elixir-yasnippets fill-column-indicator flycheck-credo flycheck-elixir fullframe go-dlv go-eldoc go-guru go-mode go-snippets haml-mode ido-completing-read+ ido-ubiquitous js2-refactor latex less-css-mode lisp-mode magit magithub markdown-mode message minibuffer miniedit mm-decode modus-operandi-theme modus-vivendi-theme nnreddit nvm org-plus-contrib prettier-js prog-mode projectile-rails protobuf-mode rainbow-delimiters rjsx-mode rvm scss-mode sicp silly-mail simple slime slime-company tex tide use-package virtualenvwrapper visual-fill web-mode wgrep wrap-region yaml-mode yasnippet-snippets))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
