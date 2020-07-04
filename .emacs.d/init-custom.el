4c2cd6ed440459476571082d7df0104f8ac595a6
Mark Simpson <verdammelt@gmail.com>
2020-07-03 22:27:26 -0400

    clean up 'safe' theme list.

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -12,7 +12,7 @@
  ;; If there is more than one, they won't work right.
  '(canlock-password "3852e20bef8b9ce61785cd355f16f621d317031e")
  '(custom-safe-themes
-   '("169c426d7a6514f8328bfa554a3885a4a01661905f2e7c3b3f8ac2468a903b8b" "1086614cad59917cc94b51065650ee00849eebcc494eaebf29d4ca9fd1be1006"))
+   '("1adfbfd37400893dbfcede6812aabf8166fafd9e2627003971c61fc89669c864" "c0350aed6dc98abdc329906a630b4cdf8ebb147cdf2a873e2648dfc0b904b2ab"))
  '(gnus-group-change-level-function 'nnreddit-update-subscription)
  '(package-selected-packages
    '(alchemist auctex bbdb browse-kill-ring change-inner cider cider-repl clj-refactor company company-go csharp-mode diminish edit-server-htmlize elisp-slime-nav elixir-credo elixir-mode elixir-yasnippets flycheck-credo flycheck-elixir fullframe go-dlv go-eldoc go-guru go-mode go-snippets haml-mode js2-refactor latex less-css-mode lisp-mode magit magithub markdown-mode message minibuffer miniedit mm-decode modus-operandi-theme modus-vivendi-theme nnreddit nvm org-plus-contrib prettier-js prog-mode projectile-rails protobuf-mode rainbow-delimiters rjsx-mode rvm scss-mode sicp silly-mail simple slime slime-company tex tide use-package virtualenvwrapper visual-fill web-mode wgrep wrap-region yaml-mode yasnippet-snippets)))
