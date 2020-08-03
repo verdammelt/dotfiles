4d5dcde1947727facbd1735244bd7b37d3fc88d9
Mark Simpson <verdammelt@gmail.com>
2020-08-03 15:48:56 -0400

    clean up package list after previous changes.

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -14,7 +14,7 @@
  '(custom-safe-themes
    '("cf7c7ea6ccd8e251a9dbafb54a7ef7e29dcd17c5b5fbd37ea7f315b6daa509b9" "87fd15a92096797894626d25d8f8a436b90ce8d97d499a98faea972944645fbd"))
  '(package-selected-packages
-   '(alchemist auctex bbdb browse-kill-ring change-inner cider cider-repl clj-refactor company company-go csharp-mode diminish edit-server-htmlize elisp-slime-nav elixir-credo elixir-mode elixir-yasnippets flycheck-credo flycheck-elixir forge fullframe go-dlv go-eldoc go-guru go-mode go-snippets haml-mode js2-refactor latex less-css-mode lisp-mode magit markdown-mode message minibuffer miniedit mm-decode modus-operandi-theme modus-vivendi-theme nnreddit nvm org-plus-contrib prettier-js prog-mode projectile-rails protobuf-mode rainbow-delimiters rjsx-mode rvm scss-mode sicp silly-mail simple slime slime-company tex tide use-package virtualenvwrapper visual-fill web-mode wgrep wrap-region yaml-mode yasnippet-snippets)))
+   '(auctex bbdb browse-kill-ring change-inner cider cider-repl clj-refactor company csharp-mode diminish edit-server-htmlize elisp-slime-nav elixir-credo forge fullframe haml-mode js2-refactor latex less-css-mode lisp-mode magit markdown-mode message minibuffer miniedit mm-decode modus-operandi-theme modus-vivendi-theme nnreddit nvm org-plus-contrib prettier-js prog-mode projectile-rails protobuf-mode rainbow-delimiters rbenv rjsx-mode rvm scss-mode sicp silly-mail simple slime slime-company tex tide use-package virtualenvwrapper visual-fill web-mode wgrep wrap-region yaml-mode yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
