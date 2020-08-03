b5ed12b5199d6482cb32a3f6061348761c59a46b
Mark Simpson <verdammelt@gmail.com>
2020-08-03 15:45:02 -0400

    Remove unused programming mode files.

--- .emacs.d/init-prog-elixir.el
+++
@@ -1,8 +0,0 @@
-(use-package elixir-mode
-  :config (add-hook 'elixir-mode-hook 'alchemist-mode))
-(use-package alchemist
-  :diminish (alchemist-mode alchemist-phoenix-mode))
-(use-package elixir-yasnippets)
-(use-package flycheck-elixir)
-(use-package flycheck-credo
-  :config (setq flycheck-elixir-credo-strict t))
