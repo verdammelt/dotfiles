0300c7cdd137156a633d3d990a3bbd6d17c90d96
Mark Simpson <verdammelt@gmail.com>
2020-10-04 16:31:25 -0400

    Correcting typescript setup esp. linting with eslint.

--- .emacs.d/init-prog-typescript.el
+++ .emacs.d/init-prog-typescript.el
@@ -1,26 +1,23 @@
-(declare-function projectile-project-root "projectile")
-(defun mjs/setup-tide ()
-  (tide-setup)
-  (eldoc-mode +1))
-
 (use-package typescript-mode
-  :hook (typescript-mode . mjs/setup-tide)
+  :hook ((typescript-mode . tide-setup))
   :config
-  (setq typescript-indent-level 2))
+  (setq typescript-indent-level 4))
 
 (use-package tide
   :config
   (setq tide-format-options
-        '(:indentSize 2 :tabSize 2)))
+        '(:indentSize 4 :tabSize 4))
+  (flycheck-add-next-checker 'typescript-tide '(warning . javascript-eslint) 'append)
+  (flycheck-add-next-checker 'tsx-tide '(warning . javascript-eslint) 'append))
 
 (use-package web-mode
   :init (progn
-          (setq standard-indent 2)
+          (setq standard-indent 4)
           (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
           (add-hook 'web-mode-hook
                     (lambda ()
                       (when (string-equal "tsx" (file-name-extension buffer-file-name))
-                        (mjs/setup-tide)))))
+                        (tide-setup)))))
   :config
-  (progn
-    (flycheck-add-mode 'typescript-tslint 'web-mode)))
+    (flycheck-add-mode 'typescript-tslint 'web-mode)
+    (flycheck-add-mode 'javascript-eslint 'web-mode))
