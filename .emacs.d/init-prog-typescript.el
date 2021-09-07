692093b40182801a5b0450e10f056cd6377c177e
Mark Simpson <verdammelt@gmail.com>
2021-09-07 09:36:30 -0400

    Remove web-mode
    Typescript mode appears to be good enough.

--- .emacs.d/init-prog-typescript.el
+++ .emacs.d/init-prog-typescript.el
@@ -1,10 +1,4 @@
 (use-package typescript-mode
-  :config (setq typescript-indent-level 4))
-
-(use-package web-mode
-  :init (progn
-          (setq standard-indent 4)
-          (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode)))
+  :mode "\\.tsx\\'"
   :config
-    (flycheck-add-mode 'typescript-tslint 'web-mode)
-    (flycheck-add-mode 'javascript-eslint 'web-mode))
+  (setq typescript-indent-level 4))
