89efdcd7a284e6eb2430c9bc79b7d3e93a494c3d
Mark Simpson <verdammelt@gmail.com>
2021-09-06 22:54:46 -0400

    Update init-custom.

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -14,7 +14,7 @@
  '(custom-safe-themes
    '("c771affc8912ccee6fc82793c31f7537063600f42b24ac8fceb55a2624035571" "cf7c7ea6ccd8e251a9dbafb54a7ef7e29dcd17c5b5fbd37ea7f315b6daa509b9" "87fd15a92096797894626d25d8f8a436b90ce8d97d499a98faea972944645fbd"))
  '(package-selected-packages
-   '(auctex bbdb browse-kill-ring change-inner circadian clj-refactor company diminish elisp-slime-nav expand-region flycheck forge fullframe icomplete-vertical less-css-mode lsp-mode lsp-ui magit miniedit mode-line-bell modus-themes multiple-cursors nvm org org-contrib org-present org-roam paredit prettier-js projectile rbenv rvm scss-mode sicp sly sly-asdf sly-quicklisp typescript-mode unicode-fonts use-package virtualenvwrapper visual-fill-column web-mode wgrep which-key wrap-region yaml-mode yasnippet yasnippet-snippets)))
+   '(auctex bbdb browse-kill-ring circadian clj-refactor company diminish elisp-slime-nav expand-region flycheck forge fullframe icomplete-vertical less-css-mode lsp-mode lsp-ui magit miniedit mode-line-bell modus-themes multiple-cursors nvm org org-contrib org-present org-roam paredit prettier-js projectile rvm scss-mode sicp sly sly-asdf sly-quicklisp typescript-mode unicode-fonts use-package virtualenvwrapper visual-fill-column web-mode wgrep which-key wrap-region yaml-mode yasnippet yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
