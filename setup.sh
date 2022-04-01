a5bf0fd75b1cf8aa4c3fb60760539898d5f97a44
Mark Simpson <verdammelt@gmail.com>
2022-04-01 08:53:02 -0400

    Update bootstrapping instructions
    Some things changed or were missed.

--- setup.sh
+++ setup.sh
@@ -8,16 +8,24 @@ ln -vs $SRC/.???* .
 ln -vs $SRC/Bin .
 
 rm .git
-chmod 0700 .fetchmailrc
 
 echo "HOMEBREW"
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-cat $SRC/Files/brew-cask-list | xargs -n 1 brew cask install
-cat $SRC/Files/brew-list | xargs -n 1 brew install
-
-echo "RVM"
-curl -sSL https://get.rvm.io | bash -s stable
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval $(/opt/homebrew/bin/brew shellenv)
+brew bundle install ~/.Brewfile
 
 echo "LAUNCHBAR SCRIPTS"
-cp Files/*.applescript ~/Library/Application Support/LaunchBar/Actions
+cp Files/*.applescript '~/Library/Application Support/LaunchBar/Actions'
+
+echo "FILE/PROCESS LIMITS"
+sudo cp Files/limit.maxfiles.plist /Library/LaunchDaemons/
+sudo launchctl load -w /Library/LaunchDaemons/limit.maxfiles.plist
+sudo cp Files/limit.maxprocs.plist /Library/LaunchDaemons/
+sudo launchctl load -w /Library/LaunchDaemons/limit.maxprocs.plist
+
+echo "POSTFIX - see README"
+
+echo "FETCHMAIL"
+chmod 0700 .fetchmailrc
+cp Files/mark.fetchmail.plist ~/Library/LaunchAgents/
+echo "See README for next steps for fetchmail
