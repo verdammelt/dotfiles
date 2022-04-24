2ca12c2fc2ab0c8da4e80dc32f3d1c029e7b56b7
Mark Simpson <verdammelt@gmail.com>
2022-04-24 13:33:47 -0400

    Update readme and scripting.

--- setup.sh
+++ setup.sh
@@ -12,20 +12,20 @@ rm .git
 echo "HOMEBREW"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval $(/opt/homebrew/bin/brew shellenv)
-brew bundle install ~/.Brewfile
-
-echo "LAUNCHBAR SCRIPTS"
-cp Files/*.applescript '~/Library/Application Support/LaunchBar/Actions'
+source ~/.bashrc
+brew bundle install --file ~/.Brewfile
+eval "$(/usr/libexec/path_helper)"
 
 echo "FILE/PROCESS LIMITS"
-sudo cp Files/limit.maxfiles.plist /Library/LaunchDaemons/
+sudo cp $SRC/Files/limit.maxfiles.plist /Library/LaunchDaemons/
 sudo launchctl load -w /Library/LaunchDaemons/limit.maxfiles.plist
-sudo cp Files/limit.maxprocs.plist /Library/LaunchDaemons/
+sudo cp $SRC/Files/limit.maxprocs.plist /Library/LaunchDaemons/
 sudo launchctl load -w /Library/LaunchDaemons/limit.maxprocs.plist
 
 echo "POSTFIX - see README"
 
 echo "FETCHMAIL"
 chmod 0700 .fetchmailrc
-cp Files/mark.fetchmail.plist ~/Library/LaunchAgents/
-echo "See README for next steps for fetchmail
+mkdir -p ~/Library/LaunchAgents
+cp $SRC/Files/mark.fetchmail.plist ~/Library/LaunchAgents/
+echo "See README for next steps for fetchmail"
