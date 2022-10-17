8a16b25918b47f73a17beea9744cf8699fab56a7
Mark Simpson <verdammelt@gmail.com>
2022-10-17 16:38:56 -0400

    Add helper skeletons for typescript components & tests

--- .emacs.d/init-prog-typescript.el
+++ .emacs.d/init-prog-typescript.el
@@ -2,3 +2,15 @@
   :mode "\\.tsx\\'"
   :config
   (setq typescript-indent-level 4))
+
+(define-auto-insert '("\\.tsx" . "TSX Component")
+  '((read-string "Component name: " nil nil (file-name-base (buffer-file-name)))
+    "import React from 'react';" \n \n
+    "export const " str " = () => <div>blah</div>;" \n \n))
+
+(define-auto-insert '("\\.test\\.tsx" . "TSX Component test")
+  '((read-string "Component name: " nil nil (file-name-base (file-name-base (buffer-file-name))))
+    "import { " str  " } from '../" str "';" \n \n
+    "describe('" str "', () => {" \n
+    > "test('fails', () => expect(true).toBe(false))" \n \n
+    "})" \n))
