a20c4251ecda5efe6a1dff8df5eed8ca8d9d0ba4
Mark Simpson <verdammelt@gmail.com>
2024-04-07 19:38:50 -0400

    Tweaking next action agendas to filter out tickler

--- .emacs.d/init-org.el
+++ .emacs.d/init-org.el
@@ -198,8 +198,8 @@
        ("rp" "projects" tags "+PROJECT=\"TRUE\"+LEVEL=2")
        ("rP" "projects (by age)" tags "+PROJECT=\"TRUE\"+LEVEL=2"
         ((org-agenda-sorting-strategy '(todo-state-up tsia-up))))
-       ("rt" "next actions"  tags-todo "-CATEGORY=\"habits\"/!-WAIT")
-       ("rT" "next actions (by age)"  tags-todo "-CATEGORY=\"habits\"/!-WAIT"
+       ("rt" "next actions"  tags-todo "-CATEGORY={habits\\|tickler}/!-WAIT")
+       ("rT" "next actions (by age)"  tags-todo "-CATEGORY={habits\\|tickler}/!-WAIT"
         ((org-agenda-sorting-strategy '(todo-state-up tsia-up))))))))
 
 (use-package org-capture
