ba0d61a797f448691c554210ad707d0dda182472
Mark Simpson <verdammelt@gmail.com>
2024-10-28 16:14:28 -0400

    WORK: PATH not getting into Emacs.app properly in Sequoia

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,6 +11,32 @@
 (defmacro if-work (then else)
   `(if *is-work-machine* ,then ,else))
 
+;; FIXME Problem with emacs 30 on MacOS Sequoia at the moment. Not getting PATH
+;; from the Info.plist file. So here we do it manually
+(if-work
+ (progn
+   (setq exec-path
+         '(
+           "/Users/mark/.nvm/versions/node/v20.2.0/bin"
+           "/Users/mark/.rbenv/shims"
+           "/Users/mark/Bin"
+           "/opt/homebrew/bin"
+           "/opt/homebrew/sbin"
+           "/usr/local/bin"
+           "/System/Cryptexes/App/usr/bin"
+           "/usr/bin"
+           "/bin"
+           "/usr/sbin"
+           "/sbin"
+           "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin"
+           "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin"
+           "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin"
+           "/Library/Apple/usr/bin"
+           "/Library/TeX/texbin"
+           "/opt/homebrew/Cellar/emacs-plus@30/30.0.93/libexec/emacs/30.0.93/aarch64-apple-darwin24.2.0"))
+   (setenv "PATH" (mapconcat 'identity exec-path ":")))
+ nil)
+
 ;;;
 ;;; Customize
 ;;;
