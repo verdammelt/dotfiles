a1da298b14cfdaf3a580858ea17d213438d6715b
Mark Simpson <verdammelt@gmail.com>
2025-01-15 15:43:37 -0500

    Update package list

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -11,18 +11,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(0blayout auctex auctex-latexmk auto-dark bbdb browse-kill-ring code-review
-              csv-mode denote denote-explore diminish elisp-slime-nav
-              emacs-everywhere flymake-eslint flymake-markdownlint forge
-              fullframe gnuplot graphviz-dot-mode haml-mode inf-ruby magit
-              magit-todos miniedit mode-line-bell modus-themes multiple-cursors
-              nvm org-contrib org-mac-link org-present paredit pdf-tools
-              prettier-js rails-log-mode rbenv robe rubocopfmt scratch scss-mode
-              sicp sly sly-asdf sly-macrostep sly-quicklisp typescript-mode
+   '(0blayout auctex auctex-latexmk auto-dark bbdb browse-kill-ring
+              code-review csv-mode denote denote-explore diminish
+              elisp-slime-nav emacs-everywhere flymake-eslint
+              flymake-markdownlint forge fullframe gnuplot
+              graphviz-dot-mode haml-mode inf-ruby jq-mode magit
+              magit-todos miniedit mode-line-bell modus-themes
+              multiple-cursors nvm org-contrib org-mac-link
+              org-present paredit pdf-tools prettier-js rails-log-mode
+              rbenv robe rubocopfmt scratch scss-mode sicp sly
+              sly-asdf sly-macrostep sly-quicklisp typescript-mode
               visual-fill-column wgrep yaml-mode))
  '(package-vc-selected-packages
-   '((code-review :url "https://github.com/phelrine/code-review.git" :branch
-                  "fix/closql-update"))))
+   '((code-review :url "https://github.com/phelrine/code-review.git"
+                  :branch "fix/closql-update"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
